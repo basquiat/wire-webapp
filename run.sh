19ba70c07730c53c2332e8fe2cd14e5a96ad53f3
Tiago Loureiro <tiago.loureiro@gmail.com>
2018-09-23 17:59:58 +0200

    Comment

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+#
+# This script will replace any and all occurrences of the backend URLs with what the
+# env vars HOSTHTTPS and HOSTSSL are. The app itself does some self env checking based
+# on whether it is loaded from zinfra.io or wire.com; that needs fixing!
+#
+
 hosthttps=${HOSTHTTPS:-'staging-nginz-https.zinfra.io'}
 hostssl=${HOSTSSL:-'staging-nginz-ssl.zinfra.io'}
 
